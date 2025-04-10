b2b5c14ac97900d5f621a80e9882d3aead44a407
Ingeno <88973001+Ign555@users.noreply.github.com>
2025-04-10 22:11:19 +0200

    Initial commit

---
+++ lib/invoice.py
@@ -0,0 +1,100 @@
+from fpdf import FPDF
+import item as it
+
+
+class Invoice:
+    
+    number = ""
+    date = ""
+    
+    companyName = ""
+    companyImmatriculation = ""
+    companyAddress = "";
+    companyPhone = "";
+    
+    customer = ""
+    customerAddress = ""
+    
+    items = []
+    
+    paidDate = "" 
+    
+    info = ""
+    
+    def __init__(self, number, date, companyName, companyImmatriculation, companyAddress, companyPhone, customer, customerAddress):
+        
+        self.number = number
+        self.date = date
+        
+        self.companyName = companyName
+        self.companyImmatriculation = companyImmatriculation
+        self.companyAddress = companyAddress
+        self.companyPhone = companyPhone
+       
+        self.customer = customer
+        self.customerAddress = companyAddress
+        
+    def addItem(self, name, price, qty):
+        
+        self.items.append([qty, it.item(name, price)])
+    
+    def exportPDF(self):
+        
+        pdf = FPDF()
+        pdf.set_auto_page_break(auto=True, margin=15)
+        pdf.add_page()
+        pdf.add_font('DejaVu', '', 'DejaVuSans.ttf', uni=True)
+        
+        pdf.set_font("Arial", "B", 16)
+        pdf.cell(180, 10, "Facture", ln=True, align="C")
+        pdf.ln(10)
+        
+        pdf.set_font("DejaVu", "", 12)
+        pdf.cell(90, 5, self.companyName, 0, align="L")
+        pdf.cell(90, 5, self.customer, 0, align="R")
+        pdf.ln()
+        pdf.cell(90, 5, f"{self.companyImmatriculation}", 0, align="L")
+        pdf.ln()
+        pdf.cell(90, 5, f"{self.companyAddress}", 0, align="L")
+        pdf.cell(90, 5, f"{self.customerAddress}", 0, align="R")
+        pdf.ln()
+        pdf.cell(90, 5, f"{self.companyPhone}", 0, align="L")
+        pdf.ln(10)
+        
+        pdf.cell(180, 5, f"N°{self.number}", ln=True, align="L")
+        pdf.cell(180, 5, f"Date : {self.date}", ln=True, align="L")
+        pdf.ln(10)
+        
+        pdf.cell(10, 10, "Qty", 1, align="L")
+        pdf.cell(100, 10, "Désignation", 1, align="L")
+        pdf.cell(30, 10, "Prix", 1, align="L")
+        pdf.cell(50, 10, "Total", 1, align="L")
+        pdf.ln()
+        
+        for item in self.items:
+            
+            pdf.cell(10, 10, f"{item[0]}", 1, align="L")
+            pdf.cell(100, 10, item[1].name, 1, align="L")
+            pdf.cell(30, 10, f"{item[1].price}", 1, align="L")
+            pdf.cell(50, 10, f"{item[1].price*item[0]}", 1, align="L")
+            
+        pdf.ln(20)
+        
+        total = 0
+        for item in self.items:
+            
+            total += item[1].price*item[0]
+            
+        pdf.cell(190, 10, f"Total : {total} €", ln=True, align="R")
+        
+        if self.paidDate != "":
+            
+            pdf.cell(190, 10, f"Payé le: {self.paidDate}", ln=True, align="L")
+        
+        
+        pdf.cell(190, 10, self.info, ln=True, align="C")
+        
+        pdf.output(f"factures-{self.number}.pdf")
+        
+        
+        
---
+++ lib/item.py
@@ -0,0 +1,10 @@
+
+class item:
+    
+    name = ""
+    price = 0
+    
+    def __init__(self, name, price):
+        item.name = name
+        item.price = price
+        
